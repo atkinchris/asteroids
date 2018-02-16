a33d08f3c2e8aca1b3ce9feff9ea8a62cb8e4a10
Chris Atkin <atkinchris@gmail.com>
2018-02-16 22:58:50 +0000

    Add link text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Application } from 'pixi.js'
 
 import Ship from './ship'
+import Link from './link'
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -9,11 +10,11 @@ const app = new Application(width, height, { antialias: true })
 document.body.appendChild(app.view)
 
 const ship = new Ship(width / 2, height / 2)
+const link = new Link(width, height)
 
 app.stage.addChild(ship)
+app.stage.addChild(link)
 
 app.ticker.add((delta) => {
   ship.update(delta)
-
-  if (ship.y < 0) ship.y = app.stage.height
 })
