a9927358c9a0c402a54fec8c0f5aea29f46b3c11
Chris Atkin <atkinchris@gmail.com>
2018-02-17 16:35:07 +0000

    Clamp ship to screen

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ const link = new Link(width, height)
 app.stage.addChild(ship)
 app.stage.addChild(link)
 
+const clamp = ({ position }) => {
+  if (position.x > width) position.x = 0
+  if (position.x < 0) position.x = width
+  if (position.y > height) position.y = 0
+  if (position.y < 0) position.y = height
+}
+
 app.ticker.add((delta) => {
   ship.update(delta)
+  clamp(ship)
 })
