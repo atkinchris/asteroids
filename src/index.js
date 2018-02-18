83c32256bcb58cfd143d592a8b13353cd7321713
Chris Atkin <atkinchris@gmail.com>
2018-02-18 11:10:44 +0000

    Create splash screen

--- src/index.js
+++ src/index.js
@@ -1,49 +1,51 @@
-import { Application } from 'pixi.js'
+import './main.css'
 
-import Bullet from './bullet'
-import Ship from './ship'
-import Splash from './splash'
-import AsteroidManager from './asteroidManager'
-import { arrayOf } from './utils/array'
+// import { Application } from 'pixi.js'
 
-const width = window.innerWidth
-const height = window.innerHeight
-const app = new Application(width, height, { antialias: true })
-document.querySelector('.canvas').appendChild(app.view)
+// import Bullet from './bullet'
+// import Ship from './ship'
+// import Splash from './splash'
+// import AsteroidManager from './asteroidManager'
+// import { arrayOf } from './utils/array'
 
-const bullets = arrayOf(10, () => new Bullet(width))
-const spawnBullet = (x, y, direction) => {
-  const bullet = bullets.shift()
-  bullet.respawn(x, y, direction)
-  bullets.push(bullet)
-}
+// const width = window.innerWidth
+// const height = window.innerHeight
+// const app = new Application(width, height, { antialias: true })
+// document.querySelector('.canvas').appendChild(app.view)
 
-const ship = new Ship(width / 2, height / 2, spawnBullet)
-const asteroids = new AsteroidManager(width, height)
-const splash = new Splash(width, height)
-splash.visible = false
+// const bullets = arrayOf(10, () => new Bullet(width))
+// const spawnBullet = (x, y, direction) => {
+//   const bullet = bullets.shift()
+//   bullet.respawn(x, y, direction)
+//   bullets.push(bullet)
+// }
 
-bullets.forEach(bullet => app.stage.addChild(bullet))
-app.stage.addChild(asteroids)
-app.stage.addChild(ship)
-app.stage.addChild(splash)
+// const ship = new Ship(width / 2, height / 2, spawnBullet)
+// const asteroids = new AsteroidManager(width, height)
+// const splash = new Splash(width, height)
+// splash.visible = false
 
-const clamp = ({ position }) => {
-  if (position.x > width) position.x = 0
-  if (position.x < 0) position.x = width
-  if (position.y > height) position.y = 0
-  if (position.y < 0) position.y = height
-}
+// bullets.forEach(bullet => app.stage.addChild(bullet))
+// app.stage.addChild(asteroids)
+// app.stage.addChild(ship)
+// app.stage.addChild(splash)
 
-app.ticker.add((delta) => {
-  ship.update(delta)
-  clamp(ship)
+// const clamp = ({ position }) => {
+//   if (position.x > width) position.x = 0
+//   if (position.x < 0) position.x = width
+//   if (position.y > height) position.y = 0
+//   if (position.y < 0) position.y = height
+// }
 
-  bullets.filter(b => b.alive).forEach((bullet) => {
-    bullet.update(delta)
-    clamp(bullet)
-    asteroids.collides(bullet)
-  })
+// app.ticker.add((delta) => {
+//   ship.update(delta)
+//   clamp(ship)
 
-  asteroids.update(delta)
-})
+//   bullets.filter(b => b.alive).forEach((bullet) => {
+//     bullet.update(delta)
+//     clamp(bullet)
+//     asteroids.collides(bullet)
+//   })
+
+//   asteroids.update(delta)
+// })
